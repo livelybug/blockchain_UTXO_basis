8508616249f5735cd2e2f3ba324f203fe3da7027
livelybug <livelybug@gmail.com>
2018-01-25 00:33:24 +0800

    W1: 0-83

--- src/main/java/w1/TxHandler.java
+++ src/main/java/w1/TxHandler.java
@@ -1,7 +1,4 @@
-package w1;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TxHandler {
 
@@ -32,62 +29,50 @@ public class TxHandler {
         System.out.println("1");
         // IMPLEMENT THIS
         // (1) all outputs claimed by {@code tx} are in the current UTXO pool,
-        for(Transaction.Input input : inputs){
+        for(int i = 0; i < tx.numInputs(); i++){
             Boolean txInUtxo = false;
-            StringBuffer sb = new StringBuffer();
-            for (byte b : input.prevTxHash) {
-                sb.append(Integer.toHexString((int) (b & 0xff)));
-            }
-            System.out.println("input hash: " + sb);
-            for(UTXO utxo : utxos) {
-                StringBuffer sb2 = new StringBuffer();
-                for (byte b : utxo.getTxHash()) {
-                    sb2.append(Integer.toHexString((int) (b & 0xff)));
-                }
-                System.out.println("utxo hash: " + sb2);
-                if(Arrays.equals(utxo.getTxHash(), input.prevTxHash)) txInUtxo = true;
-            }
-            if(!txInUtxo) return false;
+            UTXO utxo = new UTXO(tx.getInput(i).prevTxHash,tx.getInput(i).outputIndex);
+//            StringBuffer sb = new StringBuffer();
+//            for (byte b : input.prevTxHash) {
+//                sb.append(Integer.toHexString((int) (b & 0xff)));
+//            }
+//            System.out.println("input hash: " + sb);
+//            for(UTXO utxo : utxos) {
+//                StringBuffer sb2 = new StringBuffer();
+//                for (byte b : utxo.getTxHash()) {
+//                    sb2.append(Integer.toHexString((int) (b & 0xff)));
+//                }
+//                System.out.println("utxo hash: " + sb2);
+//                if(Arrays.equals(utxo.getTxHash(), input.prevTxHash)) txInUtxo = true;
+//            }
+            if(!utxoPool.contains(utxo)) return false;
+//            if(!txInUtxo) return false;
         }
 
         System.out.println("2");
         // (2) the signatures on each input of {@code tx} are valid,
-        for(Transaction.Input input : inputs){
+        UTXOPool utxoSet = new UTXOPool();
+
+        for(int i = 0; i < tx.numInputs(); i++){
+            Transaction.Input input = tx.getInput(i);
             UTXO utxo = new UTXO(input.prevTxHash,input.outputIndex);
-
-            System.out.println(utxoPool.toString());
-            System.out.println(tx.getHash());
-
-            System.out.println("input.outputIndex: " + input.outputIndex);
-
-            System.out.println(utxoPool.getTxOutput(utxo).address);
-
-            System.out.println(tx.getRawDataToSign(input.outputIndex));
-
-            System.out.println(Crypto.class);
-
-            if(!Crypto.verifySignature(
-                    utxoPool.getTxOutput(utxo).address,
-
-                    tx.getRawDataToSign(input.outputIndex),
-
+            if(!Crypto.verifySignature(utxoPool.getTxOutput(utxo).address,
+                    tx.getRawDataToSign(i),
                     input.signature)) {
                 return false;
             }
+            if (utxoSet.contains(utxo)) return false;
+            utxoSet.addUTXO(utxo, utxoPool.getTxOutput(utxo));
         }
 
         System.out.println("3");
         // (3) no UTXO is claimed multiple times by {@code tx}
-        List<byte[]> preHashes = new ArrayList<byte[]>();
-        for(Transaction.Input input : inputs){
-            byte[] preHash = input.prevTxHash;
-            for(byte[] preHash2 : preHashes) {
-//                if(preHash2.equals(preHash)) return false;
-//                else preHashes.add(preHash);
-                if(Arrays.equals(preHash2, preHash)) return false;
-                else preHashes.add(preHash);
-            }
-        }
+//        List<byte[]> preHashes = new ArrayList<byte[]>();
+//        for(Transaction.Input input : inputs){
+//            byte[] preHash = input.prevTxHash;
+//            if(preHashes.contains(preHash)) return false;
+//            else preHashes.add(preHash);
+//            }
 
         System.out.println("4");
         // (4) all of {@code tx}s output values are non-negative
@@ -107,7 +92,7 @@ public class TxHandler {
         for(Transaction.Output output : outputs){
             outputSum += output.value;
         }
-        return (outputSum >= inputSum);
+        return (outputSum <= inputSum);
     }
 
     /**
@@ -117,36 +102,38 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
-        List<Transaction> validTxs = new ArrayList<Transaction>();
+        Set<Transaction> validTxs = new HashSet<Transaction>();
         for(Transaction tx : possibleTxs) {
             if(isValidTx(tx)) {
                 ArrayList<Transaction.Input> inputs = tx.getInputs();
                 ArrayList<UTXO> utxos = this.utxoPool.getAllUTXO();
 
                 for(Transaction.Input input : inputs){
-                    Boolean txInUtxo = false;
-                    StringBuffer sb = new StringBuffer();
-                    for (byte b : input.prevTxHash) {
-                        sb.append(Integer.toHexString((int) (b & 0xff)));
-                    }
-                    System.out.println("handleTxs-input hash: " + sb);
-                    for(UTXO utxo : utxos) {
-                        StringBuffer sb2 = new StringBuffer();
-                        for (byte b : utxo.getTxHash()) {
-                            sb2.append(Integer.toHexString((int) (b & 0xff)));
-                        }
-                        System.out.println("handleTxs-utxo hash: " + sb2);
-                        if(Arrays.equals(utxo.getTxHash(), input.prevTxHash)) this.utxoPool.removeUTXO(utxo);
-                    }
+//                    Boolean txInUtxo = false;
+//                    StringBuffer sb = new StringBuffer();
+//                    for (byte b : input.prevTxHash) {
+//                        sb.append(Integer.toHexString((int) (b & 0xff)));
+//                    }
+//                    System.out.println("handleTxs-input hash: " + sb);
+//                    for(UTXO utxo : utxos) {
+//                        StringBuffer sb2 = new StringBuffer();
+//                        for (byte b : utxo.getTxHash()) {
+//                            sb2.append(Integer.toHexString((int) (b & 0xff)));
+//                        }
+//                        System.out.println("handleTxs-utxo hash: " + sb2);
+//                        if(Arrays.equals(utxo.getTxHash(), input.prevTxHash)) this.utxoPool.removeUTXO(utxo);
+//                    }
+                    UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+                    this.utxoPool.removeUTXO(utxo);
                 }
 
 
 
                 validTxs.add(tx);
-                for(Transaction.Input input : inputs){
-                    this.utxoPool.addUTXO(new UTXO(tx.getHash(), input.outputIndex), tx.getOutput(input.outputIndex));
-                }
-//                for(int i = 0 ; i < tx.numInputs() ; i++) this.utxoPool.addUTXO(new UTXO(tx.getHash(), i), tx.getOutput(i));
+//                for(Transaction.Input input : inputs){
+//                    this.utxoPool.addUTXO(new UTXO(tx.getHash(), input.outputIndex), tx.getOutput(input.outputIndex));
+//                }
+                for(int i = 0 ; i < tx.numInputs() ; i++) this.utxoPool.addUTXO(new UTXO(tx.getHash(), i), tx.getOutput(i));
             }
         }
         return validTxs.toArray(new Transaction[0]);
